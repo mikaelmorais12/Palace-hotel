dff6ee07458eadb40b469b32f3c074198aa70a61
mikaelmorais12 <131774811+mikaelmorais12@users.noreply.github.com>
2023-05-22 09:54:02 -0300

    Create cadrealizado.php

---
+++ cadrealizado.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="cadrealizado.css" media="screen"/>
+    <title>Sucesso</title>
+</head>
+<body>
+<?php
+$verdade = TRUE;
+if ($verdade == TRUE) {
+    ?>
+    <div id="flex">
+        <h2>
+            Seu cadastro foi realizado
+        </h2>
+        <img src="https://i.ibb.co/JHtvj92/png-transparent-ok-check-todo-agenda-icon-symbol-tick-to-do-gui-completed-thumbnail-removebg-preview.png" id="concluido" alt="png-transparent-ok-check-todo-agenda-icon-symbol-tick-to-do-gui-completed-thumbnail-removebg-preview">
+        <br>
+        <button id="button">EXCLUIR OU ALTERAR</button>
+        <br>
+        <button id="button">SAIR</button>
+    </div>
+    <?php } ?>
+    <br>
+    <?php
+$falso = FALSE
+if ($falso == FALSE) {
+    ?>
+    <div id="flex">
+        <h2>
+            Seu cadastro foi negado
+        </h2>
+        <img src="https://i.ibb.co/PtyBRtV/download-removebg-preview.png" id="negado" alt="download-removebg-preview">
+        <br>
+        <button id="button">EXCLUIR OU ALTERAR</button>
+        <br>
+        <button id="button">SAIR</button>
+    </div>
+    <?php } ?>
+</body>
+</html>
