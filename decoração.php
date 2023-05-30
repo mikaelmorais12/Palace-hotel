918c4c9c1920146154665437dfaf857e4dac2631
mikaelmorais12 <131774811+mikaelmorais12@users.noreply.github.com>
2023-05-30 08:43:10 -0300

    Create decoração.php

---
+++ decoração.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="fecharestadia.css" media="screen"/>
+    <title>gerarconta</title>
+</head>
+<body>
+    <div id="flex">
+        <h1>Decoração</h1>
+        <h3>Surpresa para namorado(a)</h3>
+        <img src="https://i.ibb.co/6Zr3vP3/download.jpg" id="decoracao" alt="download">
+        <br>
+        <br>
+        <h3>Lua de mel</h3>
+        <img src="https://i.ibb.co/yfHNtBP/1-suite-quarto-vermelho-ela-e-ele-motel-maraponga-18936349.jpg" id="mel" alt="1-suite-quarto-vermelho-ela-e-ele-motel-maraponga-18936349">
+</div>   
+</body>
+</html>
