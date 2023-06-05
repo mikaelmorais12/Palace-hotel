a56ba5bdcf14aa124e886a96bafce3adc4a08494
mikaelmorais12 <131774811+mikaelmorais12@users.noreply.github.com>
2023-06-05 09:49:34 -0300

    Update decoração.php

--- decoração.php
+++ decoração.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="fecharestadia.css" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="decoracao.css" media="screen"/>
     <title>gerarconta</title>
 </head>
 <body>
@@ -15,7 +15,21 @@
         <br>
         <br>
         <h3>Lua de mel</h3>
-        <img src="https://i.ibb.co/yfHNtBP/1-suite-quarto-vermelho-ela-e-ele-motel-maraponga-18936349.jpg" id="mel" alt="1-suite-quarto-vermelho-ela-e-ele-motel-maraponga-18936349">
+        <img src="https://i.ibb.co/6Zr3vP3/download.jpg" id="decoracao" alt="download">
+        <br>
+        <br>
 </div>   
+<div id="setas">
+          <div id="setaes">
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            </svg>
+            </div>
+            <div id="setadi">
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+            </svg>
+            </div>
+        </div>
 </body>
 </html>
